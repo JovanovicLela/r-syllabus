35cff51388ac232a50fa00ee0e682eaa1d75297f
Ubuntu <jovanovic.lela.00@gmail.com>
2023-02-15 17:24:24 +0000

    Setting up Google OAuth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,24 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable
+         :recoverable, :rememberable, :validatable, :trackable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
   
   rolify
+  
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+
+    # Uncomment the section below if you want users to be created if they don't exist
+    unless user
+      user = User.create(
+        email: data['email'],
+        password: Devise.friendly_token[0,20]
+        )
+    end
+    user
+  end
          
   def to_s
     email
